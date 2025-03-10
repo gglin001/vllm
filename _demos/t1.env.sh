f704c2def9210c5e5a36b63396ef1b51dcb75a1b
Allen Guo <guosonglin001@gmail.com>
2025-03-10 23:42:21 +0800

    wip

---
+++ _demos/t1.env.sh
@@ -0,0 +1,17 @@
+###############################################################################
+
+micromamba env create -n vllm python=3.12
+# micromamba env create -n vllm python=3.10
+
+micromamba activate vllm
+pip install cmake ninja
+pip install vllm
+
+######
+
+# keep `setup.py` unchanged
+
+pip uninstall vllm
+pip install --no-build-isolation -e .
+
+###############################################################################
